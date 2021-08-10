c9874515f22cebcf7caf9de56c396d5d8f2df887
zhonghui.liao <zhonghui.liao@zhongheschool.com>
2021-08-10 12:29:21 +0800

    Initial Commit

---
+++ myproject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  myproject
+//
+//  Created by Xin Liao on 2021/8/10.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
